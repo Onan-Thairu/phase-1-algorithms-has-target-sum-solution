37959e2e84a68471541f0723f423b1a8fb669cb0
OnanThairu <thairuonan@gmail.com>
2022-06-22 17:47:04 +0300

    hasTargetSum solution 1

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set()
+  for (const number of array) {
+    const complement = target - number
+    if (seenNumbers.has(complement)) return true
+    seenNumbers.add(number)
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Runtime: O(n)
 */
 
 /* 
-  Add your pseudocode here
+  Create an object to keep track of numbers we've already seen.
+  Iterate through each number in the array.
+    for the current num, identify a complement that adds to the target (comp = target - num)
+    check if any key on our object is the complement
+      if so, return true
+      otherwise, add that number to the object
+  
 */
 
 /*
@@ -29,6 +42,21 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("");
+  // Single numbers?
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4))
 }
 
 module.exports = hasTargetSum;
